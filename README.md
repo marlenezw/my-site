cf5200f367e932d6912d0726a1094fd9d2d8a12a
marlenezw <mm730@student.london.ac.uk>
2020-04-03 23:37:52 +0200

    files for my site

---
+++ README.md
@@ -0,0 +1,9 @@
+This is my personal website,
+
+See [tonybaloney.github.io](https://tonybaloney.github.io/)
+
+
+## Credits
+
+[Clean Blog](http://startbootstrap.com/template-overviews/clean-blog/) is a stylish, responsive blog theme for [Bootstrap](http://getbootstrap.com/) created by [Start Bootstrap](http://startbootstrap.com/). This theme features a blog homepage, about page, contact page, and an example post page along with a working PHP contact form.
+# marlenemhangami.com
---
+++ blog-gen.py
@@ -0,0 +1,30 @@
+import markdown
+import jinja2
+import sys
+import glob
+
+def main():
+    posts = glob.glob("blog/*.md")
+    print(posts)
+    extensions = ['extra', 'smarty', 'meta']
+    _md = markdown.Markdown(extensions=extensions, output_format='html5')
+
+    templateLoader = jinja2.FileSystemLoader(searchpath="./")
+    templateEnv = jinja2.Environment(loader=templateLoader)
+    TEMPLATE_FILE = "_blog_template.html"
+    template = templateEnv.get_template(TEMPLATE_FILE)
+
+    for post in posts:
+        print("rendering {0}".format(post))
+        url = post.replace(".md", ".html").replace("blog/", "posts/")
+        with open(post) as post_f:
+            html = _md.convert(post_f.read())
+            doc = template.render(content=html, baseurl="https://marlenemhangami.com", url=url, **_md.Meta)
+
+        post_html = url
+        with open(post_html, "w") as post_html_f:
+            post_html_f.write(doc)
+
+
+if __name__ == "__main__":
+    main()
