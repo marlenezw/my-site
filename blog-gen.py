a1d5d8a0e6fccaaab85caa747848bea1ed7298a6
marlenezw <mm730@student.london.ac.uk>
2020-04-04 10:46:32 +0200

    changed blog gen file

--- blog-gen.py
+++ blog-gen.py
@@ -1,29 +1,39 @@
 import markdown
 import jinja2
-import sys
 import glob
+from datetime import datetime
+from email.utils import formatdate, format_datetime  # for RFC2822 formatting
+
+TEMPLATE_FILE = "templates/_blog_template.html"
+BASE_URL = "https://marlenezw.github.io/my-site/"
 
 def main():
     posts = glob.glob("blog/*.md")
-    print(posts)
     extensions = ['extra', 'smarty', 'meta']
     _md = markdown.Markdown(extensions=extensions, output_format='html5')
 
-    templateLoader = jinja2.FileSystemLoader(searchpath="./")
-    templateEnv = jinja2.Environment(loader=templateLoader)
-    TEMPLATE_FILE = "_blog_template.html"
-    template = templateEnv.get_template(TEMPLATE_FILE)
+    loader = jinja2.FileSystemLoader(searchpath="./")
+    env = jinja2.Environment(loader=loader)
 
+    all_posts = []
     for post in posts:
         print("rendering {0}".format(post))
         url = post.replace(".md", ".html").replace("blog/", "posts/")
         with open(post) as post_f:
             html = _md.convert(post_f.read())
-            doc = template.render(content=html, baseurl="https://marlenezw.github.io/my-site/", url=url, **_md.Meta)
+            doc = env.get_template(TEMPLATE_FILE).render(content=html, baseurl=BASE_URL, url=url, **_md.Meta)
 
         post_html = url
         with open(post_html, "w") as post_html_f:
             post_html_f.write(doc)
+        post_date = datetime.strptime(_md.Meta['blog_publish_date'][0], "%B %d, %Y")
+        all_posts.append(dict(**_md.Meta, date=post_date, rfc2822_date=format_datetime(post_date), link="{0}{1}".format(BASE_URL, url)))
+
+    # Order blog posts by date published
+    all_posts.sort(key=lambda item: item['date'], reverse=True)
+    # Make the RSS feed
+    with open("rss.xml", "w") as rss_f:
+        rss_f.write(env.get_template(FEED_TEMPLATE_FILE).render(posts=all_posts, date=formatdate()))
 
 
 if __name__ == "__main__":
